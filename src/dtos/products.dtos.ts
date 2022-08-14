7af4a86325f7fc01b5200a63932ab5f53b73d7eb
Leonardo Mercado <lamercadob1@gmail.com>
2022-08-13 23:53:28 -0500

    validadores en dtos

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,15 +1,25 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty, IsNumber, IsString, IsUrl } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
   readonly price: number;
+
+  @IsNumber()
+  @IsNotEmpty()
   readonly stock: number;
+
+  @IsUrl()
   readonly image: string;
 }
 
-export class UpdateProductDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly stock?: number;
-  readonly image?: string;
-}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
